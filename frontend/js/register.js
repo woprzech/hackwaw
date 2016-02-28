e002f92283a01f1037870af15b32f5984ef22cb9
dominik <domodro@notebookcheck.pl>
2016-02-28 11:40:06 +0100

    Frontend: rejestracja

--- frontend/js/register.js
+++ frontend/js/register.js
@@ -1,7 +1,27 @@
 $('#register_btn').on('click', register);
+$('#registration_confirmation_ok').on('click', return_to_main);
 
 function register() {
-  token = 'x';
-  navbar_login();
-  update_content($('#menu_click')[0]);
+  $.ajax({
+    type: 'POST',
+    url: '/backend/registration',
+    dataType: 'json',
+    contentType: 'application/json',
+    data: JSON.stringify({
+      email: $('#email').val(),
+      phone: $('#phone').val(),
+      cafename: $('#name').val(),
+      address: $('#address').val()
+    }),
+    success: function(response) {
+      $('#registration_confirmation').openModal();
+    },
+    error: function() {
+      Materialize.toast('Rejestracja nie powiodła się.', 2000);
+    }
+  });
+}
+
+function return_to_main() {
+  window.location.href = '/';
 }
